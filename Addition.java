009c10ccea82b2a1c40022e517d79e0ef4fd0f3e
neeyo0308 <shindeswapnil2006@gmail.com>
2025-03-11 16:25:13 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        // Create a scanner object to take input
+        Scanner scanner = new Scanner
